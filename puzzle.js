f80d25720ba2df7afafef2c9daaca87c172faf1b
ghxst8 <157715572+ghxst8@users.noreply.github.com>
2024-01-25 10:33:56 -0500

    2

--- puzzle.js
+++ puzzle.js
@@ -1,10 +1,39 @@
 // order is blue, red, green
 
-var currTile;
+var turns = document.getElementById("turns");
 
 
-window.onload = function(){
-    
+function switchColor(curentColor){
+    if (curentColor == "red"){
+        curentColor = "green";
+    } else if (curentColor == "green"){
+        curentColor = "blue";
+    } else if (curentColor == "blue"){
+        curentColor = "red"
+    }
 }
 
-    
+function countClick1(){
+    console.log("click 1");
+    turns += 1;
+    console.log(turns);
+    switchColor
+};
+function countClick2(){
+    console.log("click 1");
+};
+function countClick3(){
+    console.log("click 1");
+};
+function countClick4(){
+    console.log("click 1");
+};
+function countClick5(){
+    console.log("click 1");
+};
+function countClick6(){
+    console.log("click 1");
+};
+function countClick7(){
+    console.log("click 1");
+};
